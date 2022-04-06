a40de1d4c2f5396a221c06663733a02700604b24
name <email>
2022-04-06 10:10:12 -0400

    Added template class for rest of game objects. Will probably need to be updated.

---
+++ src/gobj.hpp
@@ -0,0 +1,17 @@
+#ifndef __GOBJ_HPP_
+#define __GOBJ_HPP_
+
+#include <string>
+#include "gstate.hpp"
+
+class Gobj {
+public:
+    G_obj();
+    ~G_obj();
+    
+    std::string use(Gstate* state);
+    std::string look_at(Gstate* state);
+    std::string go_to(Gstate* state);
+protected:
+}
+#endif
---
+++ src/gstate.hpp
@@ -0,0 +1,11 @@
+#ifndef __GSTATE_HPP_
+#define __GSTATE_HPP_
+
+#include <vector>
+
+struct Gstate {
+    int scene = 0;
+    std::vector inv;
+};
+
+#endif
