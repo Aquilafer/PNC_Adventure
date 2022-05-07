d500edc5fc213e84b4ea51e5ec9b42769ded62bd
Thomas Beitel <thomas.beitel@stonybrook.edu>
2022-05-07 01:42:54 -0400

    Pretty pictures :) Added graphics to game (all four of them).

--- src/game.cpp
+++ src/game.cpp
@@ -4,6 +4,7 @@
 Game::Game() : m_button("CLICK ME")
 {
 	set_border_width(10);
+	set_default_size(1000, 1000);
 
 	m_button.signal_clicked().connect(sigc::mem_fun(*this, &Game::on_click));
 	add(m_button);
